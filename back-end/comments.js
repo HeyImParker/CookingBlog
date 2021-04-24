46117e24433808efd4bb795a830a741b7dbdf074
HeyImParker <patrickjpark@gmail.com>
2021-04-23 18:04:54 -0600

    Allowed for replying to comments

--- back-end/comments.js
+++ back-end/comments.js
@@ -22,26 +22,41 @@ const commentSchema = new mongoose.Schema({
         type: Date,
         default: Date.now
     },
+    parent: {
+        type: mongoose.Schema.ObjectId,
+        ref: 'Comment'
+    },
     comment: String
 });
 
 const Comment = mongoose.model('Comment', commentSchema);
 
 //upload comment
-router.post("/:title", validUser, async (req, res) => {
+router.post("/:title/title", validUser, async (req, res) => {
     if(req.body.comment == "") {
         return res.status(400).send({
             message: "No comment."
         });
     }
+    if(req.params.title == null || req.params.title == "") {
+        return res.status(400).send({
+            message: "title undefined or empty string"
+        })
+    }
     try {
         let recipe = await Recipe.findOne({
             title: req.params.title
         });
+        if(!recipe) {
+            return res.status(404).send({
+                message: "recipe not found"
+            })
+        }
         const comment = new Comment({
             comment: req.body.comment,
             user: req.user,
             recipe: recipe,
+            parent: null
         });
         await comment.save();
         return res.sendStatus(200);
@@ -51,14 +66,57 @@ router.post("/:title", validUser, async (req, res) => {
     }
 });
 
-//get list of comments for a photo
-router.get('/:title', async (req, res) => {
+//upload a sub comment
+router.post("/:id/sub", validUser, async (req, res) => {
+    if(req.body.comment == "") {
+        return res.status(400).send({
+            message: "No comment."
+        });
+    }
+    try {
+        let parentComment = await Comment.findOne({
+            _id: req.params.id
+        });
+        const comment = new Comment({
+            comment: req.body.comment,
+            user: req.user,
+            recipe: null,
+            parent: parentComment,
+        });
+        await comment.save();
+        return res.sendStatus(200);
+    } catch(error) {
+        console.log(error);
+        return res.sendStatus(500);
+    }
+});
+
+//get list of comments for a recipe
+router.get('/:title/title', async (req, res) => {
     try {
         const recipe = await Recipe.find({
             title: req.params.title
         })
         let comments = await Comment.find({
             recipe: recipe
+        }).sort({
+            created: -1
+        }).populate('user');
+        return res.send(comments);
+    } catch(error) {
+        console.log(error);
+        return res.sendStatus(500);
+    }
+});
+
+//get list of sub comments
+router.get('/:id/sub', async (req, res) => {
+    try {
+        const headComment = await Comment.find({
+            _id: req.params.id
+        })
+        let comments = await Comment.find({
+            parent: headComment
         }).sort({
             created: -1
         }).populate('user');
--- front-end/src/components/Recipe.vue
+++ front-end/src/components/Recipe.vue
@@ -37,25 +37,13 @@
     </div>
     <div class="comments">
       <div v-if="user">
-        <div class="row-spread" v-if="commenting">
-            <textarea v-model="comment" placeholder="Write your comment here"></textarea>
-            <p>Posting comment as {{user.firstName}} {{user.lastName}}</p>
-            <div class= "row-spread">
-                <button @click="toggleComment">Cancel</button>
-                <button @click="uploadComment">Post</button>
-            </div>
-        </div>
+        <CommentWrite v-if="commenting" sub="false" parent="recipeName" @upload="uploadComment" @toggle="toggleComment"></CommentWrite>
         <button v-else @click="toggleComment">Write a comment</button>
       </div>
       <div v-else>
           <p>Login to add comments</p>
       </div>
-      <div v-for="comment in comments" :key="comment._id">
-          <p></p>
-          <hr>
-          <p>{{comment.comment}}</p>
-          <h6>{{comment.user.firstName}} {{comment.user.lastName}} - {{formatDate(comment.created)}}</h6>
-      </div>
+      <CommentDisplay class="comments" v-for="comment in comments" :key="comment._id" :comment="comment"></CommentDisplay>
       <p></p>
     </div>
 </div>
@@ -65,13 +53,17 @@
 import axios from 'axios';
 import moment from 'moment';
 import TextBody from '@/components/TextBody.vue'
+import CommentWrite from '@/components/CommentWrite.vue'
+import CommentDisplay from '@/components/CommentDisplay.vue'
 export default {
     name: "Recipe",
     props: {
         recipeName: String
     },
     components: {
-        TextBody
+        TextBody,
+        CommentWrite,
+        CommentDisplay,
     },
     data() {
         return {
@@ -114,7 +106,7 @@ export default {
         },
         async getComments() {
             try {
-                let response = await axios.get('/api/recipe/comments/' + this.recipeName);
+                let response = await axios.get('/api/recipe/comments/' + this.recipeName + '/title');
                 this.comments = response.data;
             } catch (error) {
                 console.log(error);
@@ -156,16 +148,9 @@ export default {
             this.comment = "";
             this.commenting = !(this.commenting);
         },
-        async uploadComment() {
-            try {
-                await axios.post('/api/recipe/comments/' + this.recipeName, {
-                    comment: this.comment
-                });
-                this.toggleComment();
-                this.getComments();
-            } catch(error) {
-                console.log(error);
-            }
+        uploadComment() {
+            this.toggleComment();
+            this.getComments();
         },
         formatDate(date) {
             if (moment(date).diff(Date.now(), 'days') < 15)
@@ -237,6 +222,11 @@ hr {
     width: 75%;
     color: var(--black);
     margin: 0 auto;
+}
+
+.comments {
+    margin: 0 auto;
+    width: 90%;
 }
 
 @media only screen and (max-width: 400px) {
